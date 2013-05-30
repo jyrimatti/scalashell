10ac434abca9fcd8d1f68228aec2164943d8640b
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2013-05-30 09:19:58 +0300

    some fixes to xml.bat

--- xml.bat
+++ xml.bat
@@ -37,16 +37,19 @@ object P extends RegexParsers {
   def attributeValue = "[\"']".r >> { quote => ("[^" + quote + "]*").r ~ quote      ^^ { case content ~ quote => quote + content + quote }}
   def attribute      = nameStartChar ~ rep(nameChar) ~ "=" ~ attributeValue         ^^ { case n1 ~ n ~ eq ~ value => n1 + n.mkString + eq + value }
 
-  def elemOpen       = "<" ~> """[^>\s]+""".r ~ rep("\\s+".r | attribute) <~ ">"    ^^ { case name ~ attributes => (name, attributes.mkString) }
+  def elemShort      = "<" ~> """[^?>\s]+""".r ~ rep("\\s+".r | attribute) ~ "/" <~ ">"    ^^ { case name ~ attributes => (name, attributes.mkString) }
+  def elemOpen       = "<" ~> """[^?>\s]+""".r ~ rep("\\s+".r | attribute) <~ ">"    ^^ { case name ~ attributes => (name, attributes.mkString) }
   def elemClose(elemName: String) = "<" ~ "/" ~ elemName ~ ">"  
 
   def elemContent: Parser[String] = rep("[^<]+".r | elem)                           ^^ { _.mkString }
-  def elem = elemOpen >> { case (name, attrs) => elemContent <~ elemClose(name)     ^^ { case content => "<" + name + attrs + ">" + content + "</" + name + ">" } }
+  def elem = elemShort | elemOpen >> { case (name, attrs) => elemContent <~ elemClose(name)     ^^ { case content => "<" + name + attrs + ">" + content + "</" + name + ">" } }
 
   def apply(input: Reader[Char]): Unit = parse(elem, input) match {
-	  case Success(result, next) => print(result); if (!next.atEnd) apply(next)
-	  case NoSuccess(_, next) if !next.atEnd => apply(next.rest)
+	  case Success(result, next) => println(result); if (!next.atEnd) apply(next)
+	  case NoSuccess(_, next) if !next.atEnd => apply(dropUntilNewline(next))
 	  case _ =>
   }
+
+  def dropUntilNewline(input: Reader[Char]): Reader[Char] = if (input.atEnd) input else if (Set('\n','\r') contains input.first) input.rest else dropUntilNewline(input.rest)
 }
 P(new PagedSeqReader(PagedSeq.fromSource(Source.stdin)))
